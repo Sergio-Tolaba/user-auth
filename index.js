4894f1221af392a541a6a0d1cb385512aa419871
Sergio-Tolaba <tolser2019@gmail.com>
2025-07-13 19:08:22 -0300

    jwt generate token

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import authRouter from './src/routes/auth.routes.js';
 
-
-
-
 const app = express()
 const PORT = process.env.PORT
-app.use(bodyParser.json());
-// Routers
+app.use(express.json());
 app.use('/auth', authRouter);
+
+// Routers
+app.get('/', (req,res)=>{
+    res.send("Hola Mundo")
+})
+
 
 app.listen(PORT, (req, res)=>console.log(`Server running on PORT:${PORT} http://localhost:3000 `))
